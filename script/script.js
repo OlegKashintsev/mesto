5e114e725995597963d65a50638a91884c866af3
OlegKashintsev <61374964+OlegKashintsev@users.noreply.github.com>
2020-04-21 17:01:51 +0500

    4 проектная работа, 4 итерация

--- script/script.js
+++ script/script.js
@@ -3,9 +3,15 @@ const buttonEdit = document.querySelector('.profile__button_edit');
 const popup = document.querySelector('.popup');
 const container = document.querySelector('.popup__container');
 const buttonSave = document.querySelector('.popup__button_save');
+let nameInput = document.querySelector('.popup__input_name');
+let professionInput = document.querySelector('.popup__input_profession');
+let nameForm = document.querySelector('.profile__name');
+let professionForm = document.querySelector('.profile__profession');
 
 function openPopup() {
-  popup.classList.add('popup_opened')
+  popup.classList.add('popup_opened');
+  nameInput.value = nameForm.textContent;
+  professionInput.value = professionForm.textContent;
 };
 
 function closePopup() {
@@ -14,32 +20,12 @@ function closePopup() {
 
 buttonEdit.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
-
+buttonSave.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {       
-    evt.preventDefault()
-
-    let nameInput = document.querySelector('.popup__input_name')
-    let jobInput = document.querySelector('.popup__input_profession') 
-
-    nameInput = nameInput.value
-    professionInput = jobInput.value
-
-    let nameForm = document.querySelector('.profile__name') 
-    let professionForm = document.querySelector('.profile__profession')
-
-    nameForm.textContent = nameInput
-    professionForm.textContent = professionInput
+  evt.preventDefault();
+nameForm.textContent = nameInput.value;
+professionForm.textContent = professionInput.value;
 };
 
 container.addEventListener('submit', formSubmitHandler);
-
-
-function save() {
-  popup.classList.remove('popup_opened')
-};
-
-buttonSave.addEventListener('click', save);
-
-document.querySelector('.popup__input_name').value = document.querySelector('.profile__name').textContent;
-document.querySelector('.popup__input_profession').value = document.querySelector('.profile__profession').textContent;
